bfb2909632fde015642fc8ab9292416c079602fa
M Rajesh <m.rajeshrajz@gmail.com>
2020-07-17 14:10:40 +0530

    initial commit

---
+++ src/main/java/com/lwl/quizerassessment/QuizerAssessmentServerApplication.java
@@ -0,0 +1,15 @@
+package com.lwl.quizerassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class QuizerAssessmentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizerAssessmentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lwl/quizerassessment/QuizerAssessmentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwl.quizerassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizerAssessmentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
